26e757a292e7b00cce139cdd5c111f7507ff29d3
jchong1 <jonchong56@gmail.com>
2017-05-24 12:14:36 -0700

    working on histogram

--- encode.c
+++ encode.c
@@ -1,5 +1,27 @@
+# include <stdio.h>
+
 int main()
 {
 	printf("encode the darrell long dong");
 	return 0;
+}
+
+uint32_t *makeHistogram(char *file)
+{
+	FILE *fp = fopen(file, "r");
+	char c;
+	uint32_t *hist = calloc(256, sizeof(uint32_t));
+	hist[0]++;
+	hist[255]++;
+
+	if (fp == NULL)
+	{
+		perror("Couldn't open file");
+		return -1;
+	}
+	while (!feof(fp))
+	{
+		c = fgetc(fp);
+
+	}
 }
