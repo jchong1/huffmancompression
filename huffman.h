949ef8e9b0c5ee85432bab86a82a1ed6e0a1fbcd
jchong1 <jonchong56@gmail.com>
2017-05-24 16:04:21 -0700

    fixing huffman.h

--- huffman.h
+++ huffman.h
@@ -18,25 +18,25 @@ struct DAH
 }; 
 
 // New node, with symbols, leaf or not, a count associated with it
-treeNode *newNode(uint8_t s, bool l, uint64_t c);
+treeNode *newNode(uint8_t , bool , uint64_t );
 
 // Delete a single node
-treeNode *delNode(treeNode *t);
+treeNode *delNode(treeNode *);
 
 // Delete a tree
-treeNode *delTree(treeNode *t);
+treeNode *delTree(treeNode *);
 
 // Dump a Huffman tree onto a file
-void dumpTree(treeNode *t, int file);
+void dumpTree(treeNode *, int);
 
 // Build a tree from the saved tree
-treeNode *loadTree(uint8_t savedTree[]);
+treeNode *loadTree(uint8_t);
 
 // Step through a tree following the code
-int32_t *stepTree(treeNode *root, treeNode *t, uint32_t code);
+int32_t *stepTree(treeNode, treeNode, uint32_t);
 
 // Parse a Huffman tree to build codes
-void buildCode(treeNode *t, code s, code table[256]);
+void buildCode(treeNode *, code, code);
 
 static inline void delNode(treeNode *h) { free(h); return; }
 
@@ -45,6 +45,6 @@ static inline bool compare(treeNode *l, treeNode *r)
 	return l->count - r->count; // l < r if negative, l = r if 0, ...
 }
 
-treeNode *join(treeNode *l, treeNode *r); // Join two subtrees
+treeNode *join(treeNode *, treeNode *); // Join two subtrees
 
 # endif
