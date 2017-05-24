eb0b957de546eb8fe3c87be786a97cc053a80b28
Eugene Chou <Eugene@Eugenes-MacBook-Pro.local>
2017-05-24 02:45:02 -0700

    adding stack.c

---
+++ stack.c
@@ -0,0 +1,68 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdbool.h>
+
+# include "stack.h"
+
+# define INVALID 0xDeadD00d
+# define STACK_SIZE 10
+
+// Code given by DDELL
+
+// Constructor
+stack *newStack()
+{
+	stack *s = (stack *) calloc(STACK_SIZE, sizeof(stack));
+    s -> size = STACK_SIZE;
+    s -> top  = 0;
+    s -> entries = (item *) calloc(STACK_SIZE, sizeof(item));
+    return s;
+}	
+
+// Deconstructor		
+voic delStack(stack *)
+{
+	free(s -> entries); 
+	free(s);
+	return; 
+}	
+
+// Return the top item
+item pop (stack *)
+{
+	if (s -> top > 0)
+    {
+        s -> top -= 1;
+        return s -> entries[s -> top];
+    }
+    else
+    {
+        return INVALID;
+    }
+}	
+
+// Add an item to the top	
+void push(stack *, item)
+{
+	if (s -> top == s -> size)
+    {
+        s -> size *= 2;
+        s -> entries = (item *) realloc(s -> entries, s -> size * sizeof(item));
+    }
+    s -> entries[s->top] = i;
+    s -> top += 1;
+    return;
+}	
+
+// Is it empty?
+bool empty(stack *)
+{
+	return s -> top == 0;
+}
+
+// It it full?	
+bool full(stack *)
+{
+	return s -> top == 1;
+}	
