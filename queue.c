ee2eb66020e89b946025a9ec3ee15fe35e4d98fe
Eugene Chou <Eugene@eduroam-169-233-229-105.ucsc.edu>
2017-05-24 20:40:46 -0700

    finished all queue functions

--- queue.c
+++ queue.c
@@ -4,46 +4,60 @@
 # include "queue.h"
 # include "huffman.h"
 
+// Code given by DDELL
+
 queue *newQueue(uint32_t size)
 {
-	struct queue *q = malloc(sizeof(queue));
-	q->size = size;
-
-	// Allocate enough memory for the number of items you want
-	q->Q = (item *) calloc(size, sizeof(item));
-
-	// Nothing in the queue yet
-	q->head = 0;
-	q->tail = 0;
-
+	queue *q = (queue *) malloc(sizeof(queue));
+	q -> size = size;
+	q -> head = q -> tail = 0;
+	q -> Q = (item *) calloc(size, sizeof(item));
 	return q;
 }
 
 void delQueue(queue *q)
 {
-	free(q->Q);
-	free(q);
+	free(q -> Q); 
+	free(q); 
 	return;
 }
 
 // Return whether the queue is empty or full
 bool empty(queue *q)
 {
-	return (q->head == 0 && q->tail == 0);
+	return q -> head == q -> tail;
 }
 
 bool full(queue *q)
 {
-	return (q->head == q->size - 1);
+	return (q -> head + 1) % q -> size == q -> tail % q -> size;
 }
 
 
 bool enqueue(queue *q, item i)
 {
-
+	if (full(q))
+	{
+		return false;
+	}
+	else
+	{
+		q->Q[q->head] = i;
+		q->head = (q->head + 1) % q->size;
+		return true;
+	}
 }
 
 bool dequeue(queue *q, item *i)
 {
-
+	if (empty(q))
+	{
+		return false;
+	}
+	else
+	{
+		*i = q->Q[q->tail];
+		q->tail = (q->tail + 1) % q->size;
+		return true;
+	}
 }
