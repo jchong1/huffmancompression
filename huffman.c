e260b2c483cf83d3c7944626a7d01f65efeda044
Eugene Chou <Eugene@eduroam-169-233-237-65.ucsc.edu>
2017-05-31 19:02:18 -0700

    finished loadTree??

--- huffman.c
+++ huffman.c
@@ -12,7 +12,7 @@ treeNode *newNode(uint8_t s, bool l, uint64_t c)
 	// allocate space for a treeNode
 	struct treeNode *node = (listNode *) malloc(sizeof(listNode));
 	// set members of struct treeNode
-	node -> symbols = s;
+	node -> symbol = s;
 	node -> leaf = l;
 	node -> count = c;
 	node -> *right = NULL;
@@ -29,7 +29,32 @@ void dumpTree(treeNode *t, int file)
 // Build a tree from the saved tree
 treeNode *loadTree(uint8_t savedTree[], uint16_t treeBytes)
 {
-	// code
+	// Make a new stack
+	stack *s = newStack();
+	// Iterate over contents of savedTree from 0 to treeSize.
+	for (uint16_t i = 0; i < treeBytes; i++)
+	{
+		// If the element of the array is an L, then the next element will be the symbol for the leaf node. Use that
+		// symbol to create a node using newNode. Then push this new node back onto the stack.
+		if (savedTree[i] == 'L')
+		{
+			treeNode *leafNode = newNode(savedTree[i], true, 0);
+			push(s, leafNode);
+		}
+		// If the element of the array is an I, then you have encountered an interior node. 
+		// Pop once to get the right child of the interior child and then pop again to acquire the left child. 
+		// Then create the interior node using join and then push the interior node back into the stack.
+		if (savedTree[i] == 'I')
+		{
+			treeNode *right = pop(s);
+			treeNode *left = pop(s);
+			treeNode *interior = join(left, right);
+		}
+	}
+	// After you finish iterating the loop, pop one last time. 
+	// This should give you back the root of your Huffman tree.
+	treeNode *root = pop(s);
+	return root;
 }
 
 // Step through a tree following the code
@@ -53,7 +78,7 @@ uint32_t *stepTree(treeNode *root, treeNode **t, uint32_t code)
 // Parse a Huffman tree to build codes
 void buildCode(treeNode *t, code s, code table[256])
 {
-	uint32_t index = 0; 
+	static uint32_t index = 0; 
 	if (node->leaf) // if node is a leaf (base case)
 	{
 		// record current stack in code table
@@ -62,10 +87,10 @@ void buildCode(treeNode *t, code s, code table[256])
 	else
 	{
 		pushCode(s, 0);
-		constructCode(node->left);
+		buildCode(node->left);
 		popCode(s, table);
 		pushCode(s, 1);
-		constructCode(node->right);
+		buildCode(node->right);
 		popCode(s, table);
 	}
 	return;
