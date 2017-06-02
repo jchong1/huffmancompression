0b7f3eda5a671aa259b8ab6a73df872e1490c202
Eugene Chou <Eugene@eduroam-169-233-237-65.ucsc.edu>
2017-06-01 18:50:58 -0700

    tested histogram and testing harness

--- encode.c
+++ encode.c
@@ -1,39 +1,46 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdint.h>
+# include <stdlib.h>
+# include <stdbool.h>
 # include <getopt.h>
 # include <unistd.h>
-# include <stdbool.h>
+
+# include "huffman.h"
 
 # define OPTIONS "i:o:v"
 
 uint32_t *loadHistogram(char *);
+char *sFile = NULL,
+	 *oFile = NULL;
 
-int main()
+int main(int argc, char **argv)
 {
 	// Parse command-line arguments
 	int encode;
-	FILE *sFile = NULL,
-	 	 *oFile = stdout;
-	while (encode = getopt(argc, argv, OPTIONS) != -1)
+	bool iflag = false;
+	bool oflag = false;
+	while ((encode = getopt(argc, argv, OPTIONS)) != -1)
 	{
 		switch (encode)
 		{
-			case i:
+			case 'i':
 			{
-				// file being read from
+				// Set input file
+				iflag = true;
 				sFile = optarg;
 				break;
 			}
-			case o:
+			case 'o':
 			{
-				// file being written to (default stdout)
+				// Set output file
+				oflag = true;
 				oFile = optarg;
 				break;
 			}
-			case v:
+			case 'v':
 			{
-				// verbose option
+				// Verbose option
 				break;
 			}
 			default:
@@ -44,50 +51,58 @@ int main()
 		}
 	}
 
-	// 1. Compute a histogram of the file (count the number of occurrences of each byte in the file).
-	loadHistogram(sFile);
-	// 2. Construct Huffman tree that represents the histogram (use a priority queue).
-	loadTree();
-	// 3. Construct code for each symbol (use a stack and perform a traversal of the Huffman tree).
-
-	// 4. Emit encoding of Huffman tree to a file (perform a post-order traversal of the Huffman tree).
-
-	// 5. Emit encoding of original file to the compressed file (use bit vectors with operations append and concatenate).
-
-	// 6. Read the tree from the compressedfile, in order to do this you will use a stack.
-
-	// 7. Decode the compressed bitstream into an identical copy of the original file. In order to do this, you will use
-	//    the bits (0 means left, 1 means right) to guide your traversal of a reconstructed Huffman tree.
-
-// Creates a histogram of char frequencies in file
-uint32_t *loadHistogram(FILE *file)
-{
-	uint8_t c = 0; // keeps track of ascii character index
-	uint32_t *hist = calloc(256, sizeof(uint32_t));
-	if (hist) // check if calloc allocated space correctly
+	if (iflag)
 	{
-		FILE *fp = fopen(file, "r"); // open file
-		if (fp == NULL)
-		{
-			perror("Couldn't open file");
-			exit(1);
-		}
+		// 1. Compute a histogram of the file (count the number of occurrences of each byte in the file).
+		loadHistogram(sFile);
+		// 2. Construct Huffman tree that represents the histogram (use a priority queue).
 
-		// Increment first and last to ensure at least 2 elements
-		hist[0]++;
-		hist[255]++;
+		// 3. Construct code for each symbol (use a stack and perform a traversal of the Huffman tree).
 
-		// Read file
-		while (feof(fp) != 0)
-		{
-			fread(fp, &c, 1); // after character is read...
-			hist[c]++;		  // increment character frequency
-		}
-		fclose(fp); // close file
+		// 4. Emit encoding of Huffman tree to a file (perform a post-order traversal of the Huffman tree).
+
+		// 5. Emit encoding of original file to the compressed file (use bit vectors with operations append and/or concatenate).
+
+		// 6. Read the tree from the compressed file, in order to do this you will use a stack.
+
+		// 7. Decode the compressed bitstream into an identical copy of the original file. In order to do this, you will use
+		//    the bits (0 means left, 1 means right) to guide your traversal of a reconstructed Huffman tree.
 	}
 	else
 	{
-		printf("failure to calloc space for histogram");
+		printf("Please specify an input file.\n");
 	}
-	return hist;
+	return 0;
+}
+
+// Creates a histogram of char frequencies in file
+uint32_t *loadHistogram(char *file)
+{
+	// Construct histogram and increment the first and last to guarantee at 2 elements
+  	uint32_t *histogram = calloc(256, sizeof(uint32_t));
+  	histogram[0] += 1;
+ 	histogram[255] += 1;
+
+ 	// Read input file
+  	FILE *input = fopen(file, "r");
+  	int c = 0;
+  	while ((c = fgetc(input))) 
+  	{
+    	if(c == EOF) 
+    	{
+    		break;
+    	}
+    	histogram[c] += 1;
+  	}
+
+  	// printing loop to debug
+  	for (int i = 0; i < 256; i++) 
+  	{
+    	if (histogram[i] > 0) 
+    	{
+      		printf("[%c]: %d times\n", i, histogram[i]);
+    	}
+  	}
+  	fclose(input);
+  	return histogram;
 }
