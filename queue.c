89561e56ddbfd61026b53149f92a653eccd53db9
jchong1 <jonchong56@gmail.com>
2017-06-02 00:15:24 -0700

    finished queue

--- queue.c
+++ queue.c
@@ -4,7 +4,7 @@
 
 # include "queue.h"
 # include "huffman.h"
-# include "code.h"
+// # include "code.h"
 
 # ifndef MAX
 # define MAX 10
@@ -46,7 +46,7 @@ bool full(queue **q)
 }
 
 // Enqueue should put the items from smallest -> biggest, head points at smallest element
-bool enqueue(queue **head, item item)
+bool enqueue(queue **head, item *item)
 {
 	if (full(head))
 	{
@@ -54,28 +54,47 @@ bool enqueue(queue **head, item item)
 	}
 	if (empty(head))
 	{
-		*head = newLink(item);
+		printf("if empty\n");
+		*head = newLink(*item);
 		return true;
 	}
-	queue *current = *head;
+	queue *current = *head; //make new?
 	queue *nextNode;
+
+	if (item->count <= current->item.count)
+	{
+		printf("inserting at head\n");
+		queue *nL = newLink(*item);
+		nL->next = current;
+		*head = nL;
+		return true;
+	}
 
 	while (current != NULL)
 	{
+		printf("current is not null\n");
 		nextNode = current->next;
-		// If item trying to insert greater than the current node
-		if (item->count >= current->item->count)
+
+		if (nextNode == NULL)
 		{
+			printf("inserting biggest element\n");
+			current->next = newLink(*item);
+			break;
+		}
+
+		// If item trying to insert greater than the current node
+		else if (item->count >= current->item.count && item->count <= nextNode->item.count)
+		{
+			printf("item is bigger\n");
 			// Insert the item
-			current->next = newLink(item);
+			current->next = newLink(*item);
 			// Link the inserted item to the next node in the list
 			current->next->next = nextNode;
+			break;
 		}
-		else if (nextNode == NULL)
-		{
-			current->next = newLink(item);
-			current->next->next = NULL;
-		}
+
+		// If the item is the largest, make last link
+		
 		current = nextNode;
 	}
 	return true;
@@ -84,22 +103,51 @@ bool enqueue(queue **head, item item)
 // Dequeue should return the SMALLEST item
 bool dequeue(queue **head, item *item)
 {
-	if (empty(q))
+	if (empty(head))
 	{
 		return false;
 	}
 	else
 	{
-		*item = *head->item;
-		*head = *head->next;
+		// *item = (*head)->item;
+		*head = (*head)->next;
 		return true;
+	}
+}
+
+void printQueue(queue **head)
+{
+	queue *temp = *head;
+	while (temp != NULL)
+	{
+		printf("%llu\n", temp->item.count);
+		temp = temp->next;
 	}
 }
 
 int main(void)
 {
-	queue **head = malloc(sizeof(queue *));
+	srand(1);
+	treeNode *firstNode = newNode(1, false, rand() % 100);
+	treeNode *secondNode = newNode(1, false, rand() % 100);
+	treeNode *thirdNode = newNode(1, false, rand()% 100);
+	treeNode *fourthNode = newNode(1, false, rand() % 100);
+	treeNode *fifthNode = newNode(1, false, rand() % 100);
 	
+	treeNode *dequeued = NULL;
+
+
+	queue **queueHead = calloc(1, sizeof(queue *));
+
+	enqueue(queueHead, firstNode);
+	enqueue(queueHead, secondNode);
+	enqueue(queueHead, thirdNode);
+	enqueue(queueHead, fourthNode);
+	enqueue(queueHead, fifthNode);
+	dequeue(queueHead, dequeued);
+	printQueue(queueHead);
+	// printf("%llu", dequeued->count);
+	// printQueue(queueHead);
 
 	return 0;
 }
